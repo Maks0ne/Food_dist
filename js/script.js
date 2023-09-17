fb1408ea03c7e4d60f460b9ffdcb94002d80f0ab
Maks0ne <maks.goodluck23@gmail.com>
2023-09-17 13:56:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function () {
+const calculator = require('./modules/calculator')
+const cards = require('./modules/cards')
+const forms = require('./modules/forms')
+const modal = require('./modules/modal')
+const slider = require('./modules/slider')
+const tabs = require('./modules/tabs')
+const timer = require('./modules/timer')
+
+calculator()
+cards()
+forms()
+modal()
+slider()
+tabs()
+timer()
+});
+
+
+
+
+
+
